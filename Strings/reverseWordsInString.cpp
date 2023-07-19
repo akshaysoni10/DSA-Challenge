6c682c1dfcf7e386da133479f576b063773e810b
Akshay Soni <109035961+akshaysoni10@users.noreply.github.com>
2023-07-20 00:06:19 +0530

    Add files via upload

---
+++ Strings/reverseWordsInString.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        int i = 0;
+        string ans = "";
+        
+        while(i < n){
+            while(i < n && s[i] == ' ') i++;
+            if(i >= n) break;
+            int j = i + 1;
+            while(j < n && s[j] != ' ') j++;
+            string temp = s.substr(i, j - i);
+            if(ans.length() == 0){
+                ans = temp;
+            }
+            else{
+                ans = temp + " " + ans;
+            }
+            i = j + 1;
+        }
+        return ans;
+    }
+};
